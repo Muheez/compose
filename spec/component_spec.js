d08ead468cdd82a4c80a1bbd3e56c897c0630c9b
Muheez <muheezakanni@gmail.com>
2017-10-23 04:40:56 +0100

    add storage feature to component

--- spec/component_spec.js
+++ spec/component_spec.js
@@ -1,5 +1,5 @@
 //require component module
-var Component = require("../lib/component")(),
+var componentModule = require("../lib/component"),
     jsdom = require("jsdom").JSDOM;
 
 describe("instantiating a component", function () {
@@ -17,11 +17,18 @@ describe("instantiating a component", function () {
     global.window = dom.window;
     global.document = dom.window.document;
 
+    //fake CORE object
+    var CORE = {
+        components: {}
+    };
+    //get component constructor with fake CORE
+    var Component = componentModule(CORE);
+
     it("should return error if no arguments are provided", function () {
         expect(function () {
             return new Component();
         }).toThrowError("supply component class or id as reference string");
-    })
+    });
 
     it("should return an instance of component", function () {
         var p = new Component(".para");
@@ -29,5 +36,17 @@ describe("instantiating a component", function () {
 
         expect(p.refString).toEqual(".para");
         expect(pref).toEqual(p.self);
-    })
+    });
+
+    it("should throw error if element was not found", function () {
+        expect(function () {
+            return new Component(".classthatdoesnotexist");
+        }).toThrowError(Error);
+    });
+
+    it("should store component instance in CORE components object", function () {
+        var p = new Component(".para");
+        
+        expect(CORE.components.hasOwnProperty(".para")).toBe(true);
+    });
 })
